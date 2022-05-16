3a9a070f59ad2455ae1368792f4204b97ea82938
alondrapau99 <olitadelaltamar21@gmail.com>
2022-05-15 19:48:00 -0700

    GitHub Pages

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { SearchComponent } from './search/search.component';
+import { UnheroeComponent } from './unheroe/unheroe.component';
+import { DocumentalComponent } from './documental/documental.component';
+
+import {YouTubePlayerModule} from '@angular/youtube-player';//videos de youtube
+import { ComicsComponent } from './comics/comics.component';
+import { MiembrosComponent } from './miembros/miembros.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    SearchComponent,
+    UnheroeComponent,
+    DocumentalComponent,
+    ComicsComponent,
+    MiembrosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    YouTubePlayerModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/documental/documental.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-documental',
+  templateUrl: './documental.component.html',
+  styleUrls: ['./documental.component.css']
+})
+export class DocumentalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+  }
+
+}
